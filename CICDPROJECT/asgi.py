a2ab9eabdfbd9141cd1b4b8f349e5135d1b2318a
Nitin892 <78948806+Nitin892@users.noreply.github.com>
2024-11-28 12:03:10 +0530

    addd

---
+++ CICDPROJECT/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CICDPROJECT project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CICDPROJECT.settings')
+
+application = get_asgi_application()
